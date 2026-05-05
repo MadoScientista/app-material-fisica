44188b38711dc3ef9e2955901737997a86288d4c
MadoScientista <samuel.cortes.n@gmail.com>
2026-05-05 01:12:06 -0400

    Actualización método POST

--- suscripciones/src/main/java/com/madoscientista/suscripciones/controller/SuscripcionController.java
+++ suscripciones/src/main/java/com/madoscientista/suscripciones/controller/SuscripcionController.java
@@ -6,10 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.madoscientista.suscripciones.dto.RequestSuscripcionDTO;
 import com.madoscientista.suscripciones.dto.ResponseSuscripcionDTO;
 import com.madoscientista.suscripciones.mapper.SuscripcionMapper;
+import com.madoscientista.suscripciones.model.Suscripcion;
 import com.madoscientista.suscripciones.service.SuscripcionService;
 
 @RestController
@@ -41,7 +46,7 @@ public class SuscripcionController {
     }
 
     // Retorna la suscripción de un usuario por su ID
-    @GetMapping("/usuario/{idUsuario}")
+    @GetMapping("/{idUsuario}")
     public ResponseEntity<?> getSuscripcionByUsuarioId(@PathVariable Long idUsuario) {
 
         ResponseSuscripcionDTO response = suscripcionMapper.toDTO(service.getSuscripcionByUsuarioId(idUsuario));
@@ -51,9 +56,35 @@ public class SuscripcionController {
         return ResponseEntity.ok(response);
     }
 
+    // Retorna el número máximo de ejercicios permitidos para un usuario según su suscripción
+    @GetMapping("/max-ejercicios/{idUsuario}")
+    public ResponseEntity<Long> getMaxEjerciciosByUsuarioId(@PathVariable Long idUsuario) {
+        Long maxEjercicios = service.getMaxEjerciciosByUsuarioId(idUsuario);
+        return ResponseEntity.ok(maxEjercicios);
+    }
+
     // Retorna una lista de suscripciones por una lista de IDs de usuario
     // @GetMapping("/usuarios")
     // public List<ResponseSuscripcionDTO> getSuscripcionesByUsuarioIds(@RequestBody List<Long> idUsuarios) {
     //     return suscripcionMapper.toDTOList(service.getSuscripcionesByUsuarioIds(idUsuarios));
     // }
+
+
+    // ------------------------------------------------------
+    // ---------------- Sección POST ------------------------
+    // ------------------------------------------------------
+
+    // Crea una nueva suscripción para un usuario según su ID
+    @PostMapping
+    public ResponseEntity<?> postSuscripcion(@RequestBody RequestSuscripcionDTO request){
+
+        Suscripcion nuevaSuscripcion = service.postSuscripcion(request.idUsuario, request.nomrbeTipoSuscripcion);
+        ResponseSuscripcionDTO response = suscripcionMapper.toDTO(nuevaSuscripcion);
+
+        if(response == null){
+            return ResponseEntity.badRequest().body("No se pudo crear la suscripción. Verifique que el tipo de suscripción sea válido.");
+        }
+        
+        return ResponseEntity.ok(response);
+    }
 }
