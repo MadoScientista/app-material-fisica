790b91a0b1dab9bd0b1fc45a38aa9b48badd9b52
MadoScientista <samuel.cortes.n@gmail.com>
2026-05-11 22:27:58 -0400

    Corrección error en tipo de suscripción

--- suscripciones/src/main/resources/db/migration/V2__suscripciones_de_prueba.sql
+++ suscripciones/src/main/resources/db/migration/V2__suscripciones_de_prueba.sql
@@ -2,7 +2,7 @@
 -- Inserción suscripciones de prueba
 INSERT INTO suscripcion (id_usuario, activo, id_tipo_suscripcion) VALUES
 (1, 1, 1),
-(2, 2, 1),
+(2, 1, 2),
 (3, 1, 1),
 (4, 1, 1),
 (5, 1, 1),
@@ -10,6 +10,6 @@ INSERT INTO suscripcion (id_usuario, activo, id_tipo_suscripcion) VALUES
 (7, 1, 1),
 (8, 1, 1),
 (9, 1, 1),
-(10, 2, 1),
+(10, 1, 2),
 (11, 1, 1),
 (12, 1, 1);
