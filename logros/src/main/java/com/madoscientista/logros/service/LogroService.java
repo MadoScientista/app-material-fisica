2d216dab2e3c62205e9a0e9f4df63195d7f8490c
MadoScientista <samuel.cortes.n@gmail.com>
2026-05-12 22:17:31 -0400

    Corrección en guardado de logros

--- logros/src/main/java/com/madoscientista/logros/service/LogroService.java
+++ logros/src/main/java/com/madoscientista/logros/service/LogroService.java
@@ -64,7 +64,7 @@ public class LogroService {
         }
 
 
-        return listaLogros;
+        return lRepo.saveAll(listaLogros);
     }
 
     // --------------------------------------------------------
