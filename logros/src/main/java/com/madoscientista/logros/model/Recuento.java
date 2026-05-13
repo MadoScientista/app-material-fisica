71b917ce8cd7a27a35bad9aeb388b600afb977ac
MadoScientista <samuel.cortes.n@gmail.com>
2026-05-12 22:01:32 -0400

    Entidad para el registro de indicadores de logro

---
+++ logros/src/main/java/com/madoscientista/logros/model/Recuento.java
@@ -0,0 +1,31 @@
+package com.madoscientista.logros.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Recuento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idRecuento;
+
+    @Column(nullable = false)
+    private Long idUsuario;
+
+    @Column(nullable = false)
+    private Long nEjerciciosCreados;
+
+    @Column(nullable = false)
+    private Long nEjerciciosCompartidos;
+
+    @Column(nullable = false)
+    private Long nComunidades;
+}
