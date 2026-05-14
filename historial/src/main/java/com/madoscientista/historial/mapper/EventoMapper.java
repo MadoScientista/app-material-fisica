1448851866ceea166fc193415a44e7d99141ce6e
MadoScientista <samuel.cortes.n@gmail.com>
2026-05-14 11:23:42 -0400

    Ajuste dependencias

--- historial/src/main/java/com/madoscientista/historial/mapper/EventoMapper.java
+++ historial/src/main/java/com/madoscientista/historial/mapper/EventoMapper.java
@@ -1,7 +1,6 @@
 package com.madoscientista.historial.mapper;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
